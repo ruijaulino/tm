b99aba4c39ac8c7640b096084466fb7e66c37d04
RJ <32416622+ruijaulino@users.noreply.github.com>
2025-08-21 14:24:39 +0100

    first version working w lr only

--- tm/base/__init__.py
+++ tm/base/__init__.py
@@ -1,6 +1,6 @@
 from .base import BaseModel
 from .lr import LR
-from .bayes_lr import BayesLinRegr, RollVarBayesLinRegr
+from .bayes_lr import BayesLinRegr
 from .state_gaussian import StateGaussian
 from .gaussian import Gaussian, ConditionalGaussian
 from .hmm import HMM, HMMEmissions, uGaussianEmissions
